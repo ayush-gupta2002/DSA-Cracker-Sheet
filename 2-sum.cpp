752085e2a0d7921006e665afb4475dea9d984f05
ayush-gupta2002 <96584604+ayush-gupta2002@users.noreply.github.com>
2022-08-12 15:09:00 +0530

    Create 2-sum.cpp

---
+++ 2-sum.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    int getPairsCount(int arr[], int n, int k) {
+        int count = 0;
+        unordered_map<int,int> hash_map;
+        for(int i=0;i<n;i++){
+            int value = k - arr[i];
+            if(hash_map[value]){
+                count += hash_map[value];
+            }
+            hash_map[arr[i]]++;
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.getPairsCount(arr, n, k);
+        cout << ans << "\n";
+    }
+    
+    return 0;
+}
+// } Driver Code Ends

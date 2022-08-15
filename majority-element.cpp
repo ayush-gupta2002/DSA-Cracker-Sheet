275e6664cd09fd7f536918fde47d2f84273c3164
ayush-gupta2002 <96584604+ayush-gupta2002@users.noreply.github.com>
2022-08-15 16:58:25 +0530

    Create majority-element.cpp

---
+++ majority-element.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count = 0;
+        int candidate = 0;
+        
+        for(auto i:nums){
+            if(count == 0){
+                candidate = i;
+            }
+            if(candidate == i){
+                count ++;
+            }
+            else if(candidate!=i){
+                count --;
+            }
+        }
+        return candidate;
+    }
+};

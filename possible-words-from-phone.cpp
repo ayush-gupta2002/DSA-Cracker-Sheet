75e3ad0df40fbc22192371dc3d9ee222a6ad53c1
ayush-gupta2002 <96584604+ayush-gupta2002@users.noreply.github.com>
2022-09-02 22:52:14 +0530

    Create possible-words-from-phone.cpp

---
+++ possible-words-from-phone.cpp
@@ -0,0 +1,79 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+#include <string>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    
+    void solver(vector<string>& res,string currentString,int index,int a[],int N,unordered_map<int,string> mapping){
+        if(index==N){
+            res.push_back(currentString);
+            return;
+        }
+        
+        for(char i:mapping[a[index]]){
+            currentString.push_back(i);
+            solver(res,currentString,index+1,a,N,mapping);
+            currentString.pop_back();
+        }
+    }
+    
+    //Function to find list of all words possible by pressing given numbers.
+    vector<string> possibleWords(int a[], int N)
+    {
+        vector<string> res;
+        unordered_map<int,string> mapping;
+        mapping[2] = "abc";
+        mapping[3] = "def";
+        mapping[4] = "ghi";
+        mapping[5] = "jkl";
+        mapping[6] = "mno";
+        mapping[7] = "pqrs";
+        mapping[8] = "tuv";
+        mapping[9] = "wxyz";
+        
+        solver(res,"",0,a,N,mapping);
+        return res;
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+  
+	int T;
+	
+	cin >> T; //testcases
+	
+	while(T--){ //while testcases exist
+	   int N;
+	    
+	   cin >> N; //input size of array
+	   
+	   int a[N]; //declare the array
+	   
+	   for(int i =0;i<N;i++){
+	       cin >> a[i]; //input the elements of array that are keys to be pressed
+	   }
+	   
+	   Solution obj;
+	   
+	  vector <string> res = obj.possibleWords(a,N);
+	  for (string i : res) cout << i << " ";
+	   cout << endl;
+	}
+	
+	return 0;
+}
+// } Driver Code Ends

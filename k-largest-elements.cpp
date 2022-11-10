a210e7d4e042901c1689f3b0411764719131629d
ayush-gupta2002 <96584604+ayush-gupta2002@users.noreply.github.com>
2022-11-10 23:49:56 +0530

    Create k-largest-elements.cpp

---
+++ k-largest-elements.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	vector<int> kLargest(int arr[], int n, int k) {
+	    priority_queue<int, vector<int>, greater<int>> pq;
+	    
+	    int i=0;
+	    for(;i<k;i++){
+	        pq.push(arr[i]);
+	    }
+	    for(;i<n;i++){
+	        if(pq.top() < arr[i]){
+	            pq.pop();
+	            pq.push(arr[i]);
+	        }
+	    }
+	    
+	    vector<int> res(k);
+	    for(int i=0;i<k;i++){
+	        res[i] = pq.top();
+	        pq.pop();
+	    }
+	    reverse(res.begin(),res.end());
+	    return res;
+	}
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.kLargest(arr, n, k);
+        for (auto x : ans) {
+            cout << x << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends

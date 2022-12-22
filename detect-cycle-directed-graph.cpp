ba71bc6ac3b8f64dc9498b73320685e2b85e3dc5
ayush-gupta2002 <96584604+ayush-gupta2002@users.noreply.github.com>
2022-12-22 23:59:47 +0530

    Update detect-cycle-directed-graph.cpp

--- detect-cycle-directed-graph.cpp
+++ detect-cycle-directed-graph.cpp
@@ -1,3 +1,5 @@
+//USING DFS ALGO
+
 #include <bits/stdc++.h>
 
 bool dfs(int i,unordered_map<int,bool>& visited, unordered_map<int,bool>& dfsVisited,unordered_map<int,list<int>>& adjList){
@@ -40,5 +42,63 @@ int detectCycleInDirectedGraph(int n, vector < pair < int, int >> & edges) {
         }
     }
     return 0;
+    
+}
+
+//USING BFS ALGO
+#include <bits/stdc++.h>
+
+bool bfs(int i,unordered_map<int,bool>& visited,unordered_map<int,list<int>>& adjList,vector<int>& indegree,int& count){
+    queue<int> q;
+    for(int i=0;i<indegree.size();i++){
+        if(indegree[i] == 0){
+            q.push(i);
+            visited[i] = true;
+        }
+    }
+    
+    while(!q.empty()){
+        int frontNode = q.front();
+        q.pop();
+        count++;
+        visited[frontNode] = true;
+        for(int k:adjList[frontNode]){
+            indegree[k]--;
+            if(indegree[k] == 0){
+                q.push(k);
+            }
+        }
+        
+    }
+}
+
+
+int detectCycleInDirectedGraph(int n, vector < pair < int, int >> & edges) {
+    unordered_map<int,list<int>> adjList;
+    for(pair<int,int> edge:edges){
+        int u = edge.first - 1;
+        int v = edge.second - 1;
+        
+        adjList[u].push_back(v);
+    }
+    unordered_map<int,bool> visited;
+    int count = 0;
+    
+    vector<int> indegree(n);
+    for(auto i:adjList){
+        for(auto k:i.second){
+            indegree[k]++;
+        }
+    }
+    
+    for(int i=1;i<n;i++){
+        if(!visited[i]){
+            bool res = bfs(i,visited,adjList,indegree,count);
+        }
+    }
+    if(count == n){
+        return false;
+    }
+    return true;
     
 }
